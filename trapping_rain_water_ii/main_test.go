8c92a34ecec9b0bc8ff6883cfa31421f5a2c7904
doranych <dartdoran@gmail.com>
2025-01-19 11:46:17 +0400

    Trapping water II

---
+++ trapping_rain_water_ii/main_test.go
@@ -0,0 +1,27 @@
+package trapping_rain_water_ii
+
+import "testing"
+
+func Test_trapRainWater(t *testing.T) {
+	type args struct {
+		heightMap [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"", args{[][]int{{1, 4, 3, 1, 3, 2}, {3, 2, 1, 3, 2, 4}, {2, 3, 3, 2, 3, 1}}}, 4},
+		{"",
+			args{[][]int{{3, 3, 3, 3, 3}, {3, 2, 2, 2, 3}, {3, 2, 1, 2, 3}, {3, 2, 2, 2, 3}, {3, 3, 3, 3, 3}}},
+			10,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := trapRainWater(tt.args.heightMap); got != tt.want {
+				t.Errorf("trapRainWater() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
