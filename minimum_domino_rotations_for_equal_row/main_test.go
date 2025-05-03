e3227f1d40bda6adc4cf260ba89b082c7b45b0e1
doranych <dartdoran@gmail.com>
2025-05-03 20:21:57 +0400

    fix today's challenge

--- minimum_domino_rotations_for_equal_row/main_test.go
+++ minimum_domino_rotations_for_equal_row/main_test.go
@@ -1,3 +1,25 @@
 package minimum_domino_rotations_for_equal_row
 
-// TODO: Add tests
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_minDominoRotations(t *testing.T) {
+	tests := []struct {
+		name string
+		A    []int
+		B    []int
+		want int
+	}{
+		{"", []int{2, 1, 2, 4, 2, 2}, []int{5, 2, 6, 2, 3, 2}, 2},
+		{"", []int{3, 5, 1, 2, 3}, []int{3, 6, 3, 3, 4}, -1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := minDominoRotations(tt.A, tt.B)
+			assert.ElementsMatch(t, tt.want, got)
+		})
+	}
+}
